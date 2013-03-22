6700f445dbb978e891d8baeebcf79aa2a8be64da
Colin Wheeler <cocoasamurai@gmail.com>
2013-03-22 10:27:48 -0500

    commit results to a text view instead of NSLog

--- Reactive Cocoa Playground/RCPObservableViewController.m
+++ Reactive Cocoa Playground/RCPObservableViewController.m
@@ -7,9 +7,11 @@
 //
 
 #import "RCPObservableViewController.h"
+#import <ReactiveCocoa/EXTScope.h>
 
 @interface RCPObservableViewController ()
-
+@property (nonatomic,retain) NSMutableString *result;
+@property (unsafe_unretained) IBOutlet NSTextView *resultField;
 @end
 
 @implementation RCPObservableViewController
@@ -18,41 +20,69 @@
 	self = [super initWithNibName:NSStringFromClass([self class]) bundle:nil];
 	if(!self) return nil;
 	
+	_result = [NSMutableString string];
+	
 	return self;
 }
 
 - (id)initWithNibName:(NSString *)nibNameOrNil bundle:(NSBundle *)nibBundleOrNil
 {
     self = [super initWithNibName:nibNameOrNil bundle:nibBundleOrNil];
-    if (self) {
-        // Initialization code here.
-    }
+    if(!self) return nil;
+	
+	_result = [NSMutableString string];
     
     return self;
 }
 
 -(void)awakeFromNib {
+	@unsafeify(self);
+	
 	//Array Sequence
 	NSArray *array = @[ @42, @54, @64, @93, @100, @102, @50 ];
 	
 	[[[array rac_sequence] signal] subscribeNext:^(id x) {
-		NSLog(@"Next Number: %@",x);
+		@strongify(self)
+		@synchronized(self.result) {
+			[self.result appendFormat:@"Next Number: %@\n", x];
+			[self.resultField setString:self.result];
+		}
 	} completed:^{
-		NSLog(@"Completed Enumeration");
+		@strongify(self);
+		@synchronized(self.result) {
+			[self.result appendString:@"Completed Sequence Enumeration\n"];
+			[self.resultField setString:self.result];
+		}
 	}];
 	
 	//Observable that returns immediately
 	[[RACSignal return:@4] subscribeNext:^(id x) {
-		NSLog(@"Return Obserable received: %@",x);
+		@strongify(self);
+		@synchronized(self.result) {
+			[self.result appendFormat:@"Received Return Value: %@\n",x];
+			[self.resultField setString:self.result];
+		}
 	} completed:^{
-		NSLog(@"Return Observable completed");
+		@strongify(self);
+		@synchronized(self.result) {
+			[self.result appendString:@"Return Value Completed\n"];
+			[self.resultField setString:self.result];
+		}
 	}];
 	
 	//Empty Signal
 	[[RACSignal empty] subscribeNext:^(id x) {
-		NSLog(@"Empty Signal Received: %@",x);
+		@strongify(self);
+		@synchronized(self.result) {
+			[self.result appendFormat:@"Empty Signal Received: %@\n",x];
+			[self.resultField setString:self.result];
+		}
 	} completed:^{
-		NSLog(@"Empty Signal Completed");
+		@strongify(self);
+		@synchronized(self.result) {
+			[self.result appendString:@"Empty Signal Completed\n"];
+			[self.resultField setString:self.result];
+		}
 	}];
 }
 
