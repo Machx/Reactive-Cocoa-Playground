103f24850c17e2379010e3d8129a9520aafe1419
Colin Wheeler <cocoasamurai@gmail.com>
2013-07-08 10:34:03 -0500

    add RCPAsyncViewController

---
+++ Reactive Cocoa Playground/RCPAsyncViewController.h
@@ -0,0 +1,13 @@
+//
+//  RCPAsyncViewController.h
+//  Reactive Cocoa Playground
+//
+//  Created by Colin Wheeler on 7/8/13.
+//  Copyright (c) 2013 Colin Wheeler. All rights reserved.
+//
+
+#import <Cocoa/Cocoa.h>
+
+@interface RCPAsyncViewController : NSViewController
+
+@end
---
+++ Reactive Cocoa Playground/RCPAsyncViewController.m
@@ -0,0 +1,27 @@
+//
+//  RCPAsyncViewController.m
+//  Reactive Cocoa Playground
+//
+//  Created by Colin Wheeler on 7/8/13.
+//  Copyright (c) 2013 Colin Wheeler. All rights reserved.
+//
+
+#import "RCPAsyncViewController.h"
+
+@interface RCPAsyncViewController ()
+
+@end
+
+@implementation RCPAsyncViewController
+
+- (id)initWithNibName:(NSString *)nibNameOrNil bundle:(NSBundle *)nibBundleOrNil
+{
+    self = [super initWithNibName:nibNameOrNil bundle:nibBundleOrNil];
+    if (self) {
+        // Initialization code here.
+    }
+    
+    return self;
+}
+
+@end
